95e2740770560ea94ccdd585f2065a88e7e31427
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-01 19:14:45 +0530

    Booleans in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+//variables in rust
+// fn main() {
+//     let x= 1;
+//     println!("The value of x is: {x}");
+// }
+
+
+//Booleans in Rust
+
+fn main(){
+    let is_male  = true;
+    let is_above_18 = true;
+
+    if is_male{
+        println!("you are male")
+    }else{
+        println!("You are not male")
+    }
+
+    if is_male && is_above_18{
+        print!("you are a legal male")
+    }
 }
