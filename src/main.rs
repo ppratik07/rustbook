61546e600797b86a4e30772428347ef46a2a7d70
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-05-01 19:25:52 +0530

    Added mutable number

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,34 @@
 //     let x= 1;
 //     println!("The value of x is: {x}");
 // }
-
+//----------------------------------------------------------
 
 //Booleans in Rust
 
-fn main(){
-    let is_male  = true;
-    let is_above_18 = true;
+// fn main(){
+//     let is_male  = true;
+//     let is_above_18 = true;
 
-    if is_male{
-        println!("you are male")
-    }else{
-        println!("You are not male")
-    }
+//     if is_male{
+//         println!("you are male")
+//     }else{
+//         println!("You are not male")
+//     }
 
-    if is_male && is_above_18{
-        print!("you are a legal male")
-    }
-}
+//     if is_male && is_above_18{
+//         print!("you are a legal male")
+//     }
+// }
+
+//----------------------------------------------------------
+//mutable
+// fn main(){
+//     let mut x = 10;
+//     let i = 0;
+//     for i in 0..100{
+//         x = x+10;
+//     }
+//     println!("x={}",x);
+// }
+//----------------------------------------------------------
+
